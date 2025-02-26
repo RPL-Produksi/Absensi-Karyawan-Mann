877ba5a00431a18a6fdb230472f52f13c2a64de6
NaraMizaru <hilal.muhammad0807@gmail.com>
2025-02-26 20:10:53 +0700

    remove: controller not used

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,3 @@
 <?php
 
-use App\Http\Controllers\Frontend\v1\AuthController as FrontendAuthController;
-use App\Http\Controllers\Backend\v1\AuthController as BackendAuthController;
-use App\Http\Controllers\Frontend\v1\HomeController;
 use Illuminate\Support\Facades\Route;
